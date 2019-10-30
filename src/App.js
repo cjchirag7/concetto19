4cb6f7fd9c2515c9034cfe7a83502dabd9ddc9fa
cjchirag7 <cjchirag7@gmail.com>
2019-10-30 19:12:28 +0530

    Removed amplify

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import { HashRouter } from "react-router-dom";
 // import { BrowserRouter } from "react-router-dom";
 // import { Provider } from "react-redux";
 // import { ConfigureStore } from "./redux/store.js";
-import Amplify from "aws-amplify";
-import aws_exports from "./aws-exports";
+// import Amplify from "aws-amplify";
+// import aws_exports from "./aws-exports";
 import "./background.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.css";
 import "./App.css";
 // import { createBrowserHistory } from "history";
-Amplify.configure(aws_exports);
+// Amplify.configure(aws_exports);
 
 // const store = ConfigureStore();
 
@@ -23,8 +23,8 @@ function App() {
       <div id="stars2" />
       <div id="stars3" />
       <HashRouter
-        // basename={process.env.PUBLIC_URL}
-        // history={createBrowserHistory}
+      // basename={process.env.PUBLIC_URL}
+      // history={createBrowserHistory}
       >
         {
           // to be replaced by BrowserRouter
